d23ffcf178822362b7547ef9a4f6028c03bf760c
Gonçalo Ferreira <gfranjaferreira@gmail.com>
2025-08-13 01:08:22 +0100

    feat: add .gitignore and .env.example files; update Firebase configuration to use environment variables

--- taca-da-pinga/src/firebase.js
+++ taca-da-pinga/src/firebase.js
@@ -1,19 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyABTaFebI2w_5Xgh3blNulKNRVJOD_TpX4",
-  authDomain: "taca-da-pinga.firebaseapp.com",
-  projectId: "taca-da-pinga",
-  storageBucket: "taca-da-pinga.firebasestorage.app",
-  messagingSenderId: "411216916757",
-  appId: "1:411216916757:web:fd6cc75b01fb04e909391c"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
